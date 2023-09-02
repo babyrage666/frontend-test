6433518bdfe419c8eada1eb24c0f46ea6c2da254
georgy <example.gmail.com>
2023-09-02 23:47:59 +0300

    Receiving data function created + API interface added

--- app/components/content/content.tsx
+++ app/components/content/content.tsx
@@ -3,7 +3,13 @@ import { ContentProps } from './content.props';
 export default function Content({ ...props }: ContentProps): JSX.Element {
 	return (
 		<div {...props}>
-
+			<div>
+				<div>LOT</div>
+				<div>
+					<div>21049 P</div>
+					<div>Стоимость одного пассажира</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+
 import Content from './components/content/content';
 import Sidebar from './components/sidebar/sidebar';
 import styles from './page.module.css';
+import path from 'path';
+import fsPromises from 'fs/promises';
+import { IFlights } from '@/interfaces/flight.interface';
 
-// function 
+//Получение данных с локального JSON
+async function getData() {
+  const resJson = await fsPromises.readFile(path.join(process.cwd(), 'api/flights.json'));
+  const res: IFlights = JSON.parse(resJson.toString());
+  return res;
+}
 
-export default function Home(): JSX.Element {
-  console.log()
+export default async function Home() {
+  const data = await getData();
   return (
     <main className={styles.main}>
       <Sidebar />
